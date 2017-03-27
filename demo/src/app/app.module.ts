82b5c68846ce9b545db9c65832b4f346e7700a37
YairTawil <yairtawil@gmail.com>
2017-03-27 18:35:29 +0300

    update version

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import {InfiniteScrollModule} from "ng-infinitescroll";
+import {InfiniteScrollModule} from "ng-infinitescroll/infinite-scroll.module";
 
 @NgModule({
   declarations: [

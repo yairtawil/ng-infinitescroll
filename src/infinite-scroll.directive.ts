2fd03f60b7b06f110634c7feb86cb3ea73c6660e
YairTawil <yairtawil@gmail.com>
2017-11-28 22:56:03 +0200

    lint(master): add tslint for ng-infinitescroll

--- src/infinite-scroll.directive.ts
+++ src/infinite-scroll.directive.ts
@@ -1,16 +1,16 @@
-import {Directive, EventEmitter, HostListener, Output} from '@angular/core';
+import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
 
 @Directive({
-  selector: '[ansynInfiniteScroll]'
+	selector: '[infiniteScroll]'
 
 })
 export class InfiniteScrollDirective {
-  @Output("ansynInfiniteScroll") private ansynInfiniteScroll = new EventEmitter();
-
-  @HostListener("scroll", ["$event"]) scroll($event) {
-    if(Math.round($event.target.scrollTop + $event.target.clientHeight) >= $event.target.scrollHeight){
-      this.ansynInfiniteScroll.emit();
-    }
-  }
+	@Output() private infiniteScroll = new EventEmitter();
+	@HostListener('scroll', ['$event'])
+	scroll($event) {
+		if (Math.round($event.target.scrollTop + $event.target.clientHeight) >= $event.target.scrollHeight) {
+			this.infiniteScroll.emit();
+		}
+	}
 
 }
